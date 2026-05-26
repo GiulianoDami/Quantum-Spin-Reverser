895c7e0348d60c2890e32f7bf87b59a70669bcd8
GiulianoDami <38913937+GiulianoDami@users.noreply.github.com>
2026-05-26 10:44:58 +0200

    Code fix

---
+++ src/types.ts
@@ -0,0 +1,12 @@
+export type Vector3D = {
+  x: number;
+  y: number;
+  z: number;
+};
+
+export type SpinState = {
+  direction: Vector3D;
+  magnitude: number;
+};
+
+export type Probability = number;
